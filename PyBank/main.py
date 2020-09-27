f2302e393253d4a2af69f88dde551da6c707d3a1
Bannon <mbannon@anl.gov>
2020-09-27 13:18:33 -0500

    Set up print messages and got num of total months

--- PyBank/main.py
+++ PyBank/main.py
@@ -4,4 +4,24 @@ Created on Sat Sep 26 18:32:44 2020
 
 @author: mbannon
 """
+#open the file and assign budget as a list
+import csv
+with open(r"C:\Users\mbannon\Desktop\python-challenge\PyBank\Resources\budget_data.csv") as csvfile:
+    readCSV = csv.DictReader(csvfile)
+    MonthCount = 0
+    for row in readCSV:
+        MonthCount += 1
+
+
+
+
+print()
+print ()
+print("Financial Analysis")
+print("----------------------")
+print(f'Total Months: {MonthCount}' )
+print(f"Total:")
+print(f"Average Change: ")
+print(f"Greatest Increase in Profits:")
+print(f"Greatest Decreate in Profits:")
 
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -4,4 +4,12 @@ Created on Sat Sep 26 18:32:44 2020
 
 @author: mbannon
 """
+#open the file
+import csv
+with open(r"C:\Users\mbannon\Desktop\python-challenge\PyBank\Resources\budget_data.csv") as csvfile:
+    readCSV = csv.reader(csvfile, delimiter=',')
+    #for row in readCSV:
+       #print(row)
+       #print(row[0])
+       #print(row[0], row[1],)
 
