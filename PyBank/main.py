e1bffcc762b7b2492da231eeb9c359cc66126241
Bannon <mbannon@anl.gov>
2020-09-27 19:39:08 -0500

    Added code to calculate average change

--- PyBank/main.py
+++ PyBank/main.py
@@ -6,48 +6,58 @@ Created on Sat Sep 26 18:32:44 2020
 """
 #import csv module
 import csv
+import os
 
-#define variables
+#initializing variables
 ProfitIncrease = 0
 ProfitDecrease = 0
 AvgChange = 0
-ProfitTotal = 0
+tmpChange = 0
+ProfitSum = 0
 i=0
-date = []
-profit = []
+date = [] #list to contain all dates
+profit = [] #list to contain all profits
+MonthlyChange = [] #list to contain calculated change between months
 
-#store the file path associated with the file why doesn't this work?
-#file = 'C:\Users\mbannon\Desktop\python-challenge\PyBank\Resources\budget_data.csv'
 #how do you sucessfully store the header row? I had to remove it because the count was wrong
-#open the file and assign budget as a list
 
+#open the file 
 with open(r"C:\Users\mbannon\Desktop\python-challenge\PyBank\Resources\budget_data.csv") as csvfile:
     csv_reader = csv.reader(csvfile, delimiter=",")
     next(csv_reader)
     MonthCount = 0
+    MonthlyChange.append(tmpChange) #set first mont record to 0
     for row in csv_reader:
-        ProfitTotal += int(row[1])
+        ProfitSum += int(row[1])
         date.append(row[0])
         profit.append(int(row[1]))
+        #don't process if this is the first month (firstrow)
+        if MonthCount != 0:
+            tmpChange = (int(profit[MonthCount]) - int(profit[MonthCount-1]))
+            MonthlyChange.append(tmpChange)
+        AvgChange += tmpChange   
         MonthCount += 1
+            
         print(row[0])
      
 #test loop to verify lists
-for i in range(MonthCount):
-      print(f'Month: {date[i]}')
-      print(f'Profit: {profit[i]}')
+#i = 0
+#for i in range(MonthCount-1):
+#      print(f'Month: {date[i]}')
+#      print(f'Profit: {profit[i]}')
+#      print (f'Total Change: {MonthlyChange[i]}')
+     
   
 #net total amount of Profit/Losses" over the entire period
-
-
 
 print()
 print ()
 print("Financial Analysis")
 print("----------------------")
 print(f'Total Months: {MonthCount}' )
-print(f'Total: ${ProfitTotal}')
+print(f'Total: ${ProfitSum}')
 print(f"Average Change: ")
 print(f"Greatest Increase in Profits:")
 print(f"Greatest Decreate in Profits:")
 
+#export to txt file
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -6,10 +6,30 @@ Created on Sat Sep 26 18:32:44 2020
 """
 #open the file
 import csv
-with open(r"C:\Users\mbannon\Desktop\python-challenge\PyBank\Resources\budget_data.csv") as csvfile:
-    readCSV = csv.reader(csvfile, delimiter=',')
-    #for row in readCSV:
-       #print(row)
-       #print(row[0])
-       #print(row[0], row[1],)
+Winner = 0
+#List = csv_reader do I need to define the data as a list to find the members
 
+with open(r"C:\Users\mbannon\Desktop\python-challenge\PyPoll\Resources\election_data.csv") as csvfile:
+    csv_reader = csv.reader(csvfile, delimiter=',')
+    next(csv_reader)
+    VotesCount = 0
+    
+    for row in csv_reader:
+        VotesCount += 1
+     
+
+#do i first have to define it as a list? I CAN USE THIS ONCE I HAVE MY LIST OF CANDIDATES x = fruits.count("cherry") 
+     
+#Print Election Results
+print()
+print ()
+print("Election Results")
+print("-------------------------------")
+print()
+print(f'Total Votes: {VotesCount}' )
+print()
+print("-------------------------------")
+
+print("-------------------------------")
+print(f'Winner: {Winner}')
+print("-------------------------------")
