ed4df40164b0924dc6a94cbc4ef1a02b178a3f23
Bannon <mbannon@anl.gov>
2020-09-27 21:21:11 -0500

    starting to work on polling

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -4,21 +4,29 @@ Created on Sat Sep 26 18:32:44 2020
 
 @author: mbannon
 """
-#open the file
+
 import csv
 import os
 
+candidates = []
+i = 0
 Winner = 0
-#List = csv_reader do I need to define the data as a list to find the members
 
+
+#open the file
 with open(r"C:\Users\mbannon\Desktop\python-challenge\PyPoll\Resources\election_data.csv") as csvfile:
     csv_reader = csv.reader(csvfile, delimiter=',')
     next(csv_reader)
-    VotesCount = 0
-    
-    for row in csv_reader:
-        VotesCount += 1
+       VotesCount = 0
+        for row in csv_reader:
+            if row [2] == temp:
+                
+            VotesCount += 1
      
+        
+        
+#find unique candidates
+
 
 #do i first have to define it as a list? I CAN USE THIS ONCE I HAVE MY LIST OF CANDIDATES x = fruits.count("cherry") 
      
