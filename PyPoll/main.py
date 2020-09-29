68ba5243717a6f6bddb1b5a1bf4aaf4949fcfe91
Bannon <mbannon@anl.gov>
2020-09-28 21:52:38 -0500

    tallied total votes and candidate votes

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -8,29 +8,34 @@ Created on Sat Sep 26 18:32:44 2020
 import csv
 import operator
 import os
-DEBUG = True
+DEBUG = False
 temp = ""
-
+candidatevotes = []
 candidates = []
 i = 0
 Winner = 0
 tempvotes = 0
+tempcandidate = ""
 
 #open the file
 with open(r"C:\Users\mbannon\Desktop\python-challenge\PyPoll\Resources\election_data.csv") as csvfile:
     csv_reader = csv.reader(csvfile, delimiter=',')
     next(csv_reader)
     results = sorted(csv_reader,key=operator.itemgetter(2))     #how to sort csv https://www.youtube.com/watch?v=VvKn3Y7qAKs
-    #if DEBUG == True:
-    #   print(results)
     VotesCount = 0
     for row in results:
         VotesCount += 1
         tempvotes += 1
-    if DEBUG == True:
-        print("VotesCount = " + str(VotesCount))
-     
-        
+        if row[2] != tempcandidate:   #find unique candidates
+          tempcandidate = row[2]
+          candidates.append(tempcandidate)  #save new candidate name        
+          if VotesCount != 1:
+              candidatevotes.append(tempvotes)          
+              tempvotes = 0
+               
+candidatevotes.append(tempvotes) #save the last candidates vote total
+    
+             
         
 #find unique candidates
 
